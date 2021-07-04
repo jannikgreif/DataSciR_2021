653301121430a6ac8b5d69ded99aee1a82750bde
jannikgreif <75136245+jannikgreif@users.noreply.github.com>
2021-07-05 00:21:05 +0200

    update website

---
+++ shiny_wordclouds/app.R
@@ -0,0 +1,52 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(ggpubr)
+library(tidyverse)
+library(ggwordcloud)
+
+load("data.RData")
+
+ui <- fluidPage(
+    
+    # Application title
+    titlePanel("Wordclouds"),
+    
+    # Sidebar with slider inputs
+    sidebarLayout(
+        sidebarPanel(
+            selectInput("names",
+                        "Select player:",
+                        choices = data$Player %>% unique(),
+                        selected = "Clint Capela")
+        ),
+        
+        # Show a plot of the generated distribution
+        mainPanel(
+            plotOutput("wordcloudPlot")
+        )
+    )
+)
+
+
+server <- function(input, output) {
+    output$wordcloudPlot <- renderPlot(ggplot(
+        data %>% filter(Player == input$names),
+        aes(label = word, size = rel_freq_scaled, color = rel_freq_color)) + 
+            geom_text_wordcloud_area(rm_outside = TRUE) + 
+            scale_size_area(max_size = 8) + 
+            scale_color_gradient2(low = "blue", high = "red") +
+            facet_grid(~ extreme_type, scales = "free", space = "free") +
+            theme_minimal()
+        )
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
--- website/content/_index.en.md
+++ website/content/_index.en.md
@@ -2,15 +2,9 @@
 title: DataSciR
 ---
 
-# Startseite
-
+## Project Screencast
 
 https://www.youtube.com/watch?v=w7Ft2ymGmfc
 
 {{< youtube w7Ft2ymGmfc >}}
 
-
-
-{{% notice note %}}
-Automatically published and hosted thanks to [Netlify](https://www.netlify.com/). Read more about [Automated HUGO deployments with Netlify](https://www.netlify.com/blog/2015/07/30/hosting-hugo-on-netlifyinsanely-fast-deploys/)
-{{% /notice %}}
